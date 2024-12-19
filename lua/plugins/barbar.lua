998f2a073541106ba366360d00b7c6ea4de0cc51
Magno Chiabai <magnochiabai@gmail.com>
2024-12-19 12:20:38 -0300

    feat(keybinds): add keybindings for BufferPick and BufferPickDelete
    Added new keybindings:
    - `<space>b` for `:BufferPick`
    - `<space>B` for `:BufferPickDelete`
    
    feat(barbar): configure barbar plugin with additional settings
    
    Configured the barbar plugin with the following settings:
    - Disabled tabpages
    - Insert new buffers at the end
    - Enabled semantic letters
    - Configured icons for diagnostics, modified buffers, and pinned buffers

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,8 +1,9 @@
+-- Buffer plugin
 return {
   'romgrk/barbar.nvim',
   dependencies = {
-    'lewis6991/gitsigns.nvim',       -- OPTIONAL: for git status
-    'nvim-tree/nvim-web-devicons',   -- OPTIONAL: for file icons
+    'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
   init = function() vim.g.barbar_auto_setup = false end,
   opts = {
@@ -11,6 +12,23 @@ return {
     -- insert_at_start = true,
     -- …etc.
   },
-  version = '^1.0.0',   -- optional: only update when a new 1.x version is released
+  config = function()
+    require 'barbar'.setup {
+      tabpages = false,
+      insert_at_end = true,
+      semantic_letters = true,
+      icons = {
+        diagnostics = {
+          [vim.diagnostic.severity.ERROR] = { enabled = true, icon = 'ﬀ' },
+          [vim.diagnostic.severity.WARN] = { enabled = false },
+          [vim.diagnostic.severity.INFO] = { enabled = false },
+          [vim.diagnostic.severity.HINT] = { enabled = false },
+        },
+        modified = { button = '●' },
+        pinned = { button = '', filename = true },
+      },
+    }
+  end,
+  version = '^1.9.1', -- optional: only update when a new 1.x version is released
 
 }
